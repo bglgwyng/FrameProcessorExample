beb1d1b3526da5df4fa478247fe4799c2b28ee22
bglgwyng <bglgwyng@gmail.com>
2022-09-07 20:53:15 +0900

    Add bounding box.

--- App.tsx
+++ App.tsx
@@ -9,32 +9,112 @@
  */
 import 'react-native-reanimated';
 
-import React, {useEffect, useState} from 'react';
-import {Text, TouchableOpacity, useWindowDimensions, View} from 'react-native';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import {
+  Platform,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+  ViewStyle,
+} from 'react-native';
 import {
   Camera,
   CameraPermissionStatus,
+  Frame,
   useCameraDevices,
+  useCameraFormat,
   useFrameProcessor,
 } from 'react-native-vision-camera';
+import {scanFaces, Face} from 'vision-camera-face-detector';
+import {runOnJS} from 'react-native-reanimated';
+import adjustToView, {type Dimensions} from './adjustToView';
+
+const styles = StyleSheet.create({
+  boundingBox: {
+    position: 'absolute',
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+});
 
 const CameraApp = () => {
   const devices = useCameraDevices('wide-angle-camera');
-  const device = devices.front;
+  const direction: 'front' | 'back' = 'front';
+  const device = devices[direction];
+
+  const format = useCameraFormat();
+
   const {width, height} = useWindowDimensions();
 
-  const frameProcessor = useFrameProcessor(frame => {
-    'worklet';
+  const [frameDimensions, setFrameDimensions] = useState<Dimensions>();
+  const [faces, setFaces] = useState<Face[]>([]);
+
+  const handleScan = useCallback((frame: Frame, newFaces: Face[]) => {
+    const isRotated = Platform.OS === 'android';
+    setFrameDimensions(
+      isRotated
+        ? {
+            width: frame.height,
+            height: frame.width,
+          }
+        : {width: frame.width, height: frame.height},
+    );
+    setFaces(newFaces);
+    console.info(newFaces[0]?.bounds);
   }, []);
 
+  const frameProcessor = useFrameProcessor(
+    frame => {
+      'worklet';
+
+      runOnJS(handleScan)(frame, scanFaces(frame));
+    },
+    [handleScan],
+  );
+
+  const style = useMemo<StyleProp<ViewStyle>>(
+    () => ({position: 'absolute', top: 0, left: 0, width, height}),
+    [width, height],
+  );
+
   return device ? (
-    <Camera
-      device={device}
-      style={{width, height}}
-      frameProcessor={frameProcessor}
-      frameProcessorFps={5}
-      isActive
-    />
+    <View style={StyleSheet.absoluteFill}>
+      <Camera
+        device={device}
+        format={format}
+        style={style}
+        frameProcessor={frameProcessor}
+        frameProcessorFps={30}
+        isActive
+      />
+      <View style={style}>
+        {frameDimensions &&
+          (() => {
+            const mirrored = Platform.OS === 'android' && direction === 'front';
+            const {adjustRect} = adjustToView(frameDimensions, {width, height});
+
+            return faces.map((i, index) => {
+              const {left, ...others} = adjustRect(i.bounds);
+
+              return (
+                <View
+                  key={index}
+                  style={[
+                    styles.boundingBox,
+                    {
+                      ...others,
+                      [mirrored ? 'right' : 'left']: left,
+                    },
+                  ]}
+                />
+              );
+            });
+          })()}
+      </View>
+    </View>
   ) : null;
 };
 
